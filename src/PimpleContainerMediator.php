b30f35f3a6431c5e7b05e37b542a406d11840afe
Michael Cummings <mgcummings@yahoo.com>
2016-07-30 23:35:08 -0700

    Updated copyright and added strict_types to PimpleContainerMediator class. Added type hinting everywhere. Moved getServiceContainer() and $serviceContainer to class from AbstractContainerMediator and changed visibility as they are implementation details.

--- src/PimpleContainerMediator.php
+++ src/PimpleContainerMediator.php
@@ -1,12 +1,13 @@
 <?php
+declare(strict_types = 1);
 /**
  * Contains PimpleContainerMediator class.
  *
- * PHP version 5.6
+ * PHP version 7.0
  *
  * LICENSE:
  * This file is part of Event Mediator - A general event mediator (dispatcher)
- * with minimum dependencies so it is easy to drop in and use.
+ * which has minimal dependencies so it is easy to drop in and use.
  * Copyright (C) 2015-2016 Michael Cummings
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -26,15 +27,14 @@
  * Boston, MA 02111-1307 USA
  *
  * or find a electronic copy at
- * <http://www.gnu.org/licenses/>.
+ * <http://spdx.org/licenses/GPL-2.0.html>.
  *
  * You should also be able to find a copy of this license in the included
  * LICENSE file.
  *
+ * @author    Michael Cummings <mgcummings@yahoo.com>
  * @copyright 2015-2016 Michael Cummings
- * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU GPL-2.0
- * @author    Michael Cummings
- * <mgcummings@yahoo.com>
+ * @license   GPL-2.0
  */
 namespace EventMediator;
 
@@ -56,6 +56,23 @@ class PimpleContainerMediator extends AbstractContainerMediator
     {
         $this->setServiceContainer($serviceContainer);
     }
+    /** @noinspection GenericObjectTypeUsageInspection */
+    /**
+     * This method is used any time the mediator need to get the actual instance
+     * of the class for an event.
+     *
+     * Normal will only be called during actual trigger of an event since lazy
+     * loading is used.
+     *
+     * @param string $serviceName
+     *
+     * @return object
+     * @throws \LogicException
+     */
+    public function getServiceByName(string $serviceName)
+    {
+        return $this->getServiceContainer()[$serviceName];
+    }
     /**
      * This is used to bring in the service container that will be used.
      *
@@ -66,12 +83,12 @@ class PimpleContainerMediator extends AbstractContainerMediator
      *
      * @param Container|null $value
      *
-     * @return $this Fluent interface.
+     * @return ContainerMediatorInterface Fluent interface.
      * @throws \InvalidArgumentException
      *
      * @link http://pimple.sensiolabs.org/ Pimple
      */
-    public function setServiceContainer($value = null)
+    public function setServiceContainer($value = null): ContainerMediatorInterface
     {
         if (null === $value) {
             $value = new Container();
@@ -84,21 +101,21 @@ class PimpleContainerMediator extends AbstractContainerMediator
             throw new \InvalidArgumentException($mess);
         }
         $this->serviceContainer = $value;
+        return $this;
     }
     /**
-     * This method is used any time the mediator need to get the actual instance
-     * of the class for an event.
+     * Used to get the service container.
      *
-     * Normal will only be called during actual trigger of an event since lazy
-     * loading is used.
-     *
-     * @param string $serviceName
-     *
-     * @return array
-     * @throws \LogicException
+     * @return mixed
      */
-    protected function getServiceByName($serviceName)
+    private function getServiceContainer()
     {
-        return $this->getServiceContainer()[$serviceName];
+        return $this->serviceContainer;
     }
+    /**
+     * Holds the container instance to be used.
+     *
+     * @var mixed $serviceContainer
+     */
+    private $serviceContainer;
 }
