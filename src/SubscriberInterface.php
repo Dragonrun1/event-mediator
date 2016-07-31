374f17a83526c6e3402933f596aba1ff0d3e2710
Michael Cummings <mgcummings@yahoo.com>
2016-07-30 22:28:23 -0700

    Updated copyright and added strict_types to SubscriberInterface. Updated DocBlocks to reflect move from old legacy array format for events to cleaner but somewhat more verbose format that is used internally. Change in format is a Backward Compatibility(BC) break so by SemVer need new major version.

--- src/SubscriberInterface.php
+++ src/SubscriberInterface.php
@@ -1,34 +1,40 @@
 <?php
+declare(strict_types=1);
 /**
  * Contains SubscriberInterface Interface.
  *
- * PHP version 5.6
+ * PHP version 7.0
  *
  * LICENSE:
- * This file is part of event-mediator - A general event mediator (dispatcher)
+ * This file is part of Event Mediator - A general event mediator (dispatcher)
  * which has minimal dependencies so it is easy to drop in and use.
  * Copyright (C) 2015-2016 Michael Cummings
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; version 2 of the License.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, you may write to
  *
- * You should be able to find a copy of this license in the LICENSE.md file. A
- * copy of the GNU GPL should also be available in the GNU-GPL.md file.
+ * Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330
+ * Boston, MA 02111-1307 USA
  *
- * @copyright 2015-2016 Michael Cummings
- * @license   http://www.gnu.org/copyleft/lesser.html GNU LGPL
+ * or find a electronic copy at
+ * <http://spdx.org/licenses/GPL-2.0.html>.
+ *
+ * You should also be able to find a copy of this license in the included
+ * LICENSE file.
+ *
  * @author    Michael Cummings <mgcummings@yahoo.com>
+ * @copyright 2015-2016 Michael Cummings
+ * @license   GPL-2.0
  *
  * Additional licence and copyright information:
  * @copyright 2004-2014 Fabien Potencier <fabien@symfony.com>
@@ -66,21 +72,50 @@ interface SubscriberInterface
     /**
      * Returns an array of event names this subscriber wants to listen to.
      *
-     * The array keys are event names and the value can be:
+     * NOTE: The old legacy array format which was trying to emulate one found
+     * in Symfony has been dropped and replace with the much cleaner array
+     * format that is used internally.
      *
-     *  * The method name to call (priority defaults to 0)
-     *  * An array composed of the method name to call and the priority
-     *  * An array of arrays composed of the method names to call and
-     *  respective priorities, or defaults to 0 if unset
+     * The array keys are event names and the value is a list of priorities
+     * with each of them containing a list of callables for that
+     * priority/event pair.
      *
-     * For instance:
+     * Pseudo code example:
      *
-     *  * array('eventName' => 'methodName')
-     *  * array('eventName' => array('methodName', $priority))
-     *  * array('eventName' => array(array('methodName1', $priority),
-     *  array('methodName2'))
+     * <pre>
+     * [
+     *     'string eventName' => [
+     *         'int|string priority' => [
+     *             'callable callable', ...
+     *         ], ...
+     *     ], ...
+     * ]
+     * </pre>
+     *
+     * Priority is either an integer, 'first', or 'last' just as seen in addListener().
+     *
+     * Another example:
+     *
+     * <pre>
+     * [
+     *     'event1' => [
+     *         100 => [
+     *             ['\\MyNS\\event1Listener', 'listenerMethod'],
+     *             function (EventInterface $event, string $eventName, MediatorInterface $mediator) {...}
+     *         ]
+     *     ],
+     *     'event2' => [
+     *         'last' => [
+     *             '\\MyNS\\event2Listener::staticListenerMethod'
+     *         ]
+     *     ]
+     * ]
+     * </pre>
      *
      * @return array The event names to listen for
+     *
+     * @since 2.0.*-dev Changed expected format from the confusing legacy one
+     * to a new cleaner one that matches the internal format.
      */
-    public function getSubscribedEvents();
+    public function getSubscribedEvents(): array;
 }
